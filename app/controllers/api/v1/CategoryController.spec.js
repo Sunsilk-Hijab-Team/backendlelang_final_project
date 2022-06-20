0b2297caa534b8052be37bb44f25a0c97dd92720
ALI FAHRIAL ANWAR <alifahrialanwar@gmail.com>
2022-06-20 17:31:38 +0700

    Update Test

---
+++ app/controllers/api/v1/CategoryController.spec.js
@@ -0,0 +1,51 @@
+const CategoryController = require('./CategoryController');
+
+describe('CategoryController', () => {
+
+    describe('#handleAdd', () => {
+
+        it('Should return 201 code and message', async () => {
+
+            const category = {
+                id: 1,
+                name: 'Jam Tangan',
+                slug: 'jam-tangan',
+            }
+
+            const mockRequest = { body: category }
+
+            const mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            }
+
+            const mockNext = jest.fn()
+
+            const categoryController = new CategoryController();
+
+            await categoryController.handleAdd(mockRequest, mockResponse, mockNext)
+
+            expect(mockResponse.status).toBeCalledWith(201)
+            expect(mockResponse.json).toBeCalledWith({
+                status: 'SUCCESS',
+                message: 'Category added successfully',
+            });
+
+        });
+
+        it('Should return error 409 and message', async () => {
+            const category = {
+                id: 1,
+                name: 'Jam Tangan',
+                slug: 'jam-tangan',
+            }
+
+            const mockRequest = { body: category }
+
+
+
+        });
+
+    });
+
+});
