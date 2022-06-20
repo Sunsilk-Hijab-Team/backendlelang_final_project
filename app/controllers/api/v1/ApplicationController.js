55d69b893cbeec55b718584bf040dd882be0741d
ALI FAHRIAL ANWAR <alifahrialanwar@gmail.com>
2022-06-20 17:11:44 +0700

    Update

--- app/controllers/api/v1/ApplicationController.js
+++ app/controllers/api/v1/ApplicationController.js
@@ -8,5 +8,4 @@ class ApplicationController{
     }
 
 }
-
 module.exports = ApplicationController;
---
+++ app/controllers/api/v1/AuthenticationController.js
@@ -0,0 +1,4 @@
+const ApplicationController = require("./ApplicationController");
+class AuthenticationController extends ApplicationController{
+
+}
