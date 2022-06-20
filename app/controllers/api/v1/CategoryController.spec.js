872df23580727d563c19ffb3abaf1583cf9e8a43
ALI FAHRIAL ANWAR <alifahrialanwar@gmail.com>
2022-06-20 22:36:34 +0700

    Update

--- app/controllers/api/v1/CategoryController.spec.js
+++ app/controllers/api/v1/CategoryController.spec.js
@@ -9,7 +9,6 @@ describe('CategoryController', () => {
             const category = {
                 id: 1,
                 name: 'Jam Tangan',
-                slug: 'jam-tangan',
             }
 
             const mockRequest = { body: category }
@@ -42,7 +41,54 @@ describe('CategoryController', () => {
 
             const mockRequest = { body: category }
 
+            const mockResponse =  {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            }
 
+            const mockNext = jest.fn()
+
+            const categoryController = new CategoryController();
+
+            await categoryController.handleAdd(mockRequest, mockResponse, mockNext)
+
+            expect(mockResponse.status).toBeCalledWith(409)
+            expect(mockResponse.json).toBeCalledWith({
+                status: 'error',
+                message: 'Category already exists',
+            })
+
+        });
+
+    });
+
+    describe('#hanldeUpdate', () => {
+
+        it('Should return 200 code and message', async () => {
+
+            const category = {
+                id: 1,
+                name: 'Jam Tangan',
+            }
+
+            const mockRequest = { body: category }
+
+            const mockResponse = {
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn().mockReturnThis(),
+            }
+
+            const mockNext = jest.fn()
+
+            const categoryController = new CategoryController();
+
+            await categoryController.handleUpdate(mockRequest, mockResponse, mockNext)
+
+            expect(mockResponse.status).toBeCalledWith(200)
+            expect(mockResponse.json).toBeCalledWith({
+                status: 'SUCCESS',
+                message: 'Category updated successfully',
+            });
 
         });
 
