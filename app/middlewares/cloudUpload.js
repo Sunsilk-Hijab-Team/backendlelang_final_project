3ba61d92502944ddf6a3d1ce7a124e2d9536eb98
ALI FAHRIAL ANWAR <alifahrialanwar@gmail.com>
2022-07-03 05:59:27 +0700

    Updated: .env to cloudUpload

--- app/middlewares/cloudUpload.js
+++ app/middlewares/cloudUpload.js
@@ -1,12 +1,13 @@
 //cloudinar uploaded image config
+require("dotenv").config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: 'dckrcrjyo',
-    api_key: 843749812497697,
-    api_secret: 'V9dMajJ6r-9UP5B5BKS3BL6kF6E',
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
     secure: true
 });
-console.log(cloudinary.config());
+// console.log(cloudinary.config());
 
 module.exports = cloudinary;
