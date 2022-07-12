b1dc56863a4cc90f5844c0a3bfc343994c5115b9
triyasniko <triyasniko@gmail.com>
2022-07-12 17:36:58 +0700

    change dbconfig

--- config/database.js
+++ config/database.js
@@ -5,10 +5,10 @@
 
 /** Destruct environment variable to get database configuration */
 const {
-  DB_USERNAME = "lwglnjmbuxpgvs",
-  DB_PASSWORD = "7d649ef2210e0b46eeefa8401b2ae2b9a8c9869bd529bdb2de8c5fbf389c9a96",
-  DB_HOST = "ec2-3-217-14-181.compute-1.amazonaws.com",
-  DB_NAME = "d4vtttg8k8nb8j",
+  DB_USERNAME = "avmceybcqbfkvu",
+  DB_PASSWORD = "f97e51dab27d5b4cca8e0be868b37a0a43776bf33d15820022eb5a1cc05f444b",
+  DB_HOST = "ec2-3-223-169-166.compute-1.amazonaws.com",
+  DB_NAME = "d7ion06unri46j",
 } = process.env;
 
 module.exports = {
