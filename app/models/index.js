d98baf5c97c35531d77d6ec94c2c70ebfee8a1f5
triyasniko <triyasniko@gmail.com>
2022-07-14 17:47:41 +0700

    update config

--- app/models/index.js
+++ app/models/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-console.log(env, "<<<<");
+// console.log(env, "<<<<");
 const config = require(__dirname + "/../../config/database.js")[env];
 const db = {};
 
